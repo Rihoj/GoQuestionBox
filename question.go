3a2550902acb167c027fd7a5d8d5200be3c20987
James Ray <jray@five-startech.com>
2018-05-18 14:23:13 -0400

    Init commit

---
+++ question.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var name string
+	var age int
+	fmt.Println("Name and age: ")
+	if _, err := fmt.Scan(&name, &age); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("Your name is: %s\n", name)
+	fmt.Printf("Your age is: %d\n", age)
+}
